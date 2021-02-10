9ca88f1b8a065db9fb863ced81f0b3240c020abf
Alistair Hampton <alidhampton@gmail.com>
2021-02-10 15:55:06 +0000

    Added Basic Visits Cookie

--- rango/views.py
+++ rango/views.py
@@ -4,21 +4,26 @@ from django.shortcuts import render, redirect, reverse
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
+from datetime import datetime
 
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
+    visits = int(request.COOKIES.get('visits', '1'))
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!',
                     'categories': category_list,
-                    'pages': page_list}
+                    'pages': page_list,
+                    'visits': visits}
 
-    return render(request, 'rango/index.html', context=context_dict)
+    response = render(request, 'rango/index.html', context=context_dict)
+    visitor_cookie_handler(request, response)
+
+    return response
 
 
 def about(request):
     context_dict = {'author': 'Alistair Hampton'}
-
     return render(request, 'rango/about.html', context=context_dict)
 
 
@@ -148,3 +153,16 @@ def user_logout(request):
     logout(request)
 
     return redirect(reverse('rango:index'))
+
+
+def visitor_cookie_handler(request, response):
+    visits = int(request.COOKIES.get('visits', '1'))
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                        '%Y-%m-%d %H:%M:%S')
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        response.set_cookie('last_visit', str(datetime.now()))
+    else:
+        response.set_cookie('last_visit', last_visit_cookie)
+    response.set_cookie('visits', visits)
